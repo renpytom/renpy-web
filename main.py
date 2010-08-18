4930365e84b7c3ccc1eda615c50b9d041bf2ca1c
Tom <tom@rothamel.us>
2010-08-18 00:10:05 -0400

    Initial runnable version.

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/<name>.html")
+def page(name):
+    """
+    Matches html-like pages and renders them using a template.
+    """
+
+    return render_template(name + ".html")
+
+@app.route("/")
+def index():
+    """
+    Renders the index page.
+    """
+
+    return page("index")
+
+if __name__ == "__main__":
+    app.run()
