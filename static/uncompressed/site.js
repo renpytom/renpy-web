ffd8983b2dd5335c138e3b848f3b929b05e69477
Tom <tom@rothamel.us>
2010-12-17 00:44:56 -0500

    Revert to synchronous GA code.

--- static/uncompressed/site.js
+++ static/uncompressed/site.js
@@ -10,18 +10,13 @@ var Site = this.Site = {
     start: function() {
         if ($('search')) this.fixSearch();
         if ($('downloads')) this.filterDownloads();
-        this.startAnalytics();
     },
 
     filterDownloads: function(showAll) {
         $$('.download-button').each(function(item) {
             if (!showAll && !item.hasClass(Browser.Platform.name)) item.addClass('inactive');
         }).addEvent('click', function() {
-            try {            
-                var pageTracker = _gat._getTracker("UA-1855234-1-1");
-                pageTracker._trackEvent('File', 'Download', this.get('href'));
-            } catch (e) {
-            }            
+            pageTracker._trackPageview(this.get('href'));            
         });
     },
 
@@ -29,11 +24,6 @@ var Site = this.Site = {
         new OverText('search');
     },
 
-    startAnalytics: function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    }
 };
 
 window.addEvent('domready', Site.start.bind(Site));
