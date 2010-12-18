0d8f8c3823b127a8ccb238fac6344572e7567f55
Incendium <incendium@gmail.com>
2010-12-18 12:16:29 -0600

    Cleaned up analytics code.

--- static/uncompressed/site.js
+++ static/uncompressed/site.js
@@ -1,29 +1,30 @@
 (function() {
 
-// analytics queue
-var _gaq = this._gaq = _gaq || [
-    ['_setAccount', 'UA-1855234-1'],
-    ['_trackPageview']
-];
+var pageTracker;
 
 var Site = this.Site = {
     start: function() {
+        this.runAnalytics();
         if ($('search')) this.fixSearch();
         if ($('downloads')) this.filterDownloads();
+    },
+
+    runAnalytics: function() {
+        pageTracker = _gat._getTracker("UA-1855234-1");
+        pageTracker._trackPageview();
     },
 
     filterDownloads: function(showAll) {
         $$('.download-button').each(function(item) {
             if (!showAll && !item.hasClass(Browser.Platform.name)) item.addClass('inactive');
         }).addEvent('click', function() {
-            pageTracker._trackPageview(this.get('href'));            
+            if (pageTracker) pageTracker._trackPageview(this.get('href'));
         });
     },
 
     fixSearch: function() {
         new OverText('search');
-    },
-
+    }
 };
 
 window.addEvent('domready', Site.start.bind(Site));
