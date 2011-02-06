284e63cb6a7f7a8231491f1e96ad8bff51561487
Tom <tom@rothamel.us>
2011-02-05 21:28:56 -0500

    6.12.0d

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ Release(
     prerelease=True,
     version="6.12.0",
     name="Positronic Brain",
-    revision="c",
+    revision="d",
     date="February xx, 2010",
     exe=21,
     zip=27,
@@ -32,6 +32,10 @@ support added in 6.11 has been improved, and new games now use screens
 by default.
 """,
     history="""\
+**2011-02-05**
+    Fourth pre-release (6.12.0d). Adds main menu prediction, fixes
+    screen bugs, improves the default screens, and fixes documentation.
+
 **2011-02-03**
     Third pre-release (6.12.0c). Updates are documented `here <http://lemmasoft.renai.us/forums/viewtopic.php?f=32&t=9207&p=121140#p121140>`_.
 
