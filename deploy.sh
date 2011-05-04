8a0ce801524834dac8482a663f22e67b2f9d792e
Tom Rothamel <tom@rothamel.us>
2011-05-03 20:20:55 -0400

    Deployed.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ try () {
     "$@" || exit -1
 }
 
+bzr commit -m 'Deployed.'
+
 try bzr push bzr+ssh://tom@onegeek.org/home/tom/renpy-web-2011/
 try ssh tom@onegeek.org bzr update /home/tom/renpy-web-2011/
 try ssh tom@onegeek.org touch /home/tom/renpy-web-2011/main.py
