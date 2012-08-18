590d73a28d624c30e5bec5d49f5daa3030decc17
Tom Rothamel <tom@rothamel.us>
2012-08-18 11:16:39 -0400

    Use fabric to deploy the website.

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import run, put, env, cd, lcd
+from fabric.contrib.project import rsync_project
+
+env.hosts = [ 'tom@erika.onegeek.org' ]
+
+
+def deploy():
+    cd("/home/tom/")
+    lcd("/home/tom/ab/web-2011")
+    
+    rsync_project(
+        local_dir="/home/tom/ab/web-2011/",
+        remote_dir="/home/tom/wsgi.renpyorg/",
+        exclude=[ ".*" ],
+        )
+
+    run("touch wsgi.renpyorg/main.wsgi")
+    
