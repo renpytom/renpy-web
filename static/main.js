f1ea6608c87ae6ef79c217c99519a3c330102d3f
Tom Rothamel <tom@rothamel.us>
2014-09-04 01:33:16 -0400

    Change button colors.
    We use success for positive things (download of stable), and warning
    for risky things (prerelease downloads).
    
    This makes important links more obvious, compared to the basic blue
    links.

--- static/main.js
+++ static/main.js
@@ -9,16 +9,22 @@ $(function () {
     });
 
 
+    var btn_class = "btn-success";
+
+    if ($("#prerelease-warning").size() > 0) {
+    	btn_class = "btn-warning";
+    }
+
     if ($.browser.linux) {
-    	$(".download-button.linux").removeClass("btn-other").addClass("btn-primary");
+    	$(".download-button.linux").removeClass("btn-other").addClass(btn_class);
     } else if ($.browser.mac) {
-    	$(".download-button.mac").removeClass("btn-other").addClass("btn-primary");
+    	$(".download-button.mac").removeClass("btn-other").addClass(btn_class);
     } else if ($.browser.win) {
-    	$(".download-button.win").removeClass("btn-other").addClass("btn-primary");
+    	$(".download-button.win").removeClass("btn-other").addClass(btn_class);
     } else {
-    	$(".download-button.linux").removeClass("btn-other").addClass("btn-primary");
-    	$(".download-button.mac").removeClass("btn-other").addClass("btn-primary");
-    	$(".download-button.win").removeClass("btn-other").addClass("btn-primary");
+    	$(".download-button.linux").removeClass("btn-other").addClass(btn_class);
+    	$(".download-button.mac").removeClass("btn-other").addClass(btn_class);
+    	$(".download-button.win").removeClass("btn-other").addClass(btn_class);
     }
 
 });
