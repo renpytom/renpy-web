9dafc783cbd17476102ed22186e3ae31d3302fcd
Tom Rothamel <tom@rothamel.us>
2021-06-08 18:33:30 -0400

    Faster deploy.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $(realpath $0))
 
-rsync --progress -a /home/tom/.virtualenvs/renpyweb/ tom@abagail.onegeek.org:/home/tom/virtualenv.renpyorg/
-rsync --exclude .\* --progress -a /home/tom/ab/magnetic/web-2011/ tom@abagail.onegeek.org:/home/tom/wsgi.renpyorg/
-ssh tom@abagail.onegeek.org touch ~/wsgi.renpyorg/main.wsgi
+rsync --progress -a /home/tom/.virtualenvs/renpyweb/ tom@192.168.99.2:/home/tom/virtualenv.renpyorg/
+rsync --exclude .\* --progress -a /home/tom/ab/magnetic/web-2011/ tom@192.168.99.2:/home/tom/wsgi.renpyorg/
+ssh tom@192.168.99.2 touch ~/wsgi.renpyorg/main.wsgi
