333567588849391469e982d54967d53bcbbc279e
Tom Rothamel <tom@rothamel.us>
2025-03-22 16:10:28 -0400

    Delete featured files on upload.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ cd $(dirname $(realpath $0))
 
 rsync --progress -a /home/tom/.virtualenvs/renpyweb/ tom@192.168.99.2:/home/tom/virtualenv.renpyorg/
 rsync --exclude .\* --progress -a /home/tom/ab/magnetic/web-2011/ tom@192.168.99.2:/home/tom/wsgi.renpyorg/
+rsync --delete --exclude .\* --progress -a /home/tom/ab/magnetic/web-2011/static/featured/ tom@192.168.99.2:/home/tom/wsgi.renpyorg/static/featured/
 ssh tom@abagail.onegeek.org touch ~/wsgi.renpyorg/main.wsgi
